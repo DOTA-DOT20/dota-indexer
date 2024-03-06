16af6a444a4288ce460aa85f701e3830f1f8549d
weaver <jyaymusk@proton.me>
2024-03-06 11:49:00 +0800

    update

--- test.py
+++ test.py
@@ -12,6 +12,7 @@
 # for i in ii:
 #     i["a"] = 100
 # print(ii)
+import json
 
 d = [{"d": 1}, {"d": 5}, {"d": 5}, {"d": 6}, {"d": 6}]
 for i in range(6, 1000):
@@ -19,22 +20,27 @@ for i in range(6, 1000):
 print(d[:0])
 
 
-def test(ite: str, hh: list[dict], result: list[list[dict]]):
-    start = hh[0].get(ite)
-    for id, h in enumerate(hh):
-        print(h)
-        print(h.items())
-        value = h.get(ite)
-        if value != start:
-            a, b = hh[:id], hh[id:]
-            print(id)
-            print("b:", b)
-            result.append(a)
-            print(result)
-            return test(ite, b, result)
-        if id == len(hh) - 1:
-            result.append(hh)
-            return result
+# def test(ite: str, hh: list[dict], result: list[list[dict]]):
+#     start = hh[0].get(ite)
+#     for id, h in enumerate(hh):
+#         print(h)
+#         print(h.items())
+#         value = h.get(ite)
+#         if value != start:
+#             a, b = hh[:id], hh[id:]
+#             print(id)
+#             print("b:", b)
+#             result.append(a)
+#             print(result)
+#             return test(ite, b, result)
+#         if id == len(hh) - 1:
+#             result.append(hh)
+#             return result
 
-print(test("d", d, []))
+# print(test("d", d, []))
 # for id, dd in enumerate(d):
+
+a = {}
+print(json.dumps(a))
+if a == dict():
+    print("haahha")
